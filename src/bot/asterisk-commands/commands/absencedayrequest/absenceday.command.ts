b9fe5d36fe96939b558815f37332bf5fa5263f7f
trung.vuquang <trung.vuquang@ncc.asia>
2024-12-12 12:58:07 +0700

    Update reply in channel, send notification to PM

--- src/bot/asterisk-commands/commands/absencedayrequest/absenceday.command.ts
+++ src/bot/asterisk-commands/commands/absencedayrequest/absenceday.command.ts
@@ -26,6 +26,11 @@ export class RequestAbsenceDayCommand extends CommandMessage {
   }
 
   async execute(args: string[], message: ChannelMessage) {
+    const messageid = message.message_id;
+    const clanId = message.clan_id;
+    const codeMess = message.code;
+    const modeMess = message.mode;
+    const isPublic = message.is_public;
     const typeRequest = args[0];
     if (!typeRequest) return;
     const typeRequestDayEnum = ERequestAbsenceDayType[typeRequest.toUpperCase() as keyof typeof ERequestAbsenceDayType];
@@ -347,7 +352,7 @@ export class RequestAbsenceDayCommand extends CommandMessage {
       {
         components: [
           {
-            id: `${typeRequestDayEnum}_CANCEL`,
+            id: `${typeRequestDayEnum}_${messageid}_${clanId}_${modeMess}_${codeMess}_${isPublic}_CANCEL`,
             type: EMessageComponentType.BUTTON,
             component: {
               label: `Cancel`,
@@ -355,7 +360,7 @@ export class RequestAbsenceDayCommand extends CommandMessage {
             },
           },
           {
-            id: `${typeRequestDayEnum}_CONFIRM`,
+            id: `${typeRequestDayEnum}_${messageid}_${clanId}_${modeMess}_${codeMess}_${isPublic}_CONFIRM`,
             type: EMessageComponentType.BUTTON,
             component: {
               label: `Confirm`,
