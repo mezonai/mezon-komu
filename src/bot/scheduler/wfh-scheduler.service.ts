e0d32e67762fe60c2208c2e896f67c4f69834682
michael.mai <michael.mai.thatera@gmail.com>
2025-05-19 18:02:20 +0700

    fix bugs

--- src/bot/scheduler/wfh-scheduler.service.ts
+++ src/bot/scheduler/wfh-scheduler.service.ts
@@ -161,7 +161,7 @@ export class WFHSchedulerService {
     }
   }
 
-  // @Cron('*/1 9-11,13-17 * * 1-5', { timeZone: 'Asia/Ho_Chi_Minh' })
+  @Cron('*/1 9-11,13-17 * * 1-5', { timeZone: 'Asia/Ho_Chi_Minh' })
   async punish() {
     if (await this.utilsService.checkHoliday()) return;
     if (this.utilsService.checkTime(new Date())) return;
