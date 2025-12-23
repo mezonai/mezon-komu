22e43aa10b8c898f9b766aae1e076bd68363cc22
michael.mai <michael.mai.thatera@gmail.com>
2025-12-23 09:09:07 +0700

    comment poll

--- src/bot/asterisk-commands/commands/poll/poll.command.ts
+++ src/bot/asterisk-commands/commands/poll/poll.command.ts
@@ -12,39 +12,39 @@ import { PollTrackerService } from 'src/bot/services/PollTracker.services';
 
 // @Command('poll')
 export class PollCommand extends CommandMessage {
-  protected client: MezonClient;
+  // protected client: MezonClient;
   constructor(
     private clientService: MezonClientService,
     private pollService: PollService,
     private pollTrackerService: PollTrackerService
   ) {
     super();
-    this.client = this.clientService.getClient();
+    // this.client = this.clientService.getClient();
   }
 
   async execute(args: string[], message: ChannelMessage, commandName?: string) {
-    const clan = this.client.clans.get(message.clan_id!);
-    const channel = await clan?.channels.fetch(message.channel_id);
-    const messageChannel = await  channel?.messages.fetch(message.message_id!);;
-    const defaultNumberOption = 2;
-    const color = getRandomColor();
-    const embed: EmbedProps[] = [
-      {
-        color,
-        title: `Poll Configuration`,
-        fields: this.pollService.generateFieldsCreatePoll(defaultNumberOption),
-        timestamp: new Date().toISOString(),
-        footer: MEZON_EMBED_FOOTER,
-      },
-    ];
-    const components = this.pollService.generateComponentsCreatePoll(
-      defaultNumberOption,
-      color,
-      (message.clan_nick || message.username) ?? '',
-      message.clan_id ?? '',
-      message.sender_id
-    );
-    const sent = await messageChannel?.reply({ embed, components });
-    this.pollTrackerService.startTrackPoll(message.clan_id, message.channel_id, sent.message_id);
+    // const clan = this.client.clans.get(message.clan_id!);
+    // const channel = await clan?.channels.fetch(message.channel_id);
+    // const messageChannel = await  channel?.messages.fetch(message.message_id!);;
+    // const defaultNumberOption = 2;
+    // const color = getRandomColor();
+    // const embed: EmbedProps[] = [
+    //   {
+    //     color,
+    //     title: `Poll Configuration`,
+    //     fields: this.pollService.generateFieldsCreatePoll(defaultNumberOption),
+    //     timestamp: new Date().toISOString(),
+    //     footer: MEZON_EMBED_FOOTER,
+    //   },
+    // ];
+    // const components = this.pollService.generateComponentsCreatePoll(
+    //   defaultNumberOption,
+    //   color,
+    //   (message.clan_nick || message.username) ?? '',
+    //   message.clan_id ?? '',
+    //   message.sender_id
+    // );
+    // const sent = await messageChannel?.reply({ embed, components });
+    // this.pollTrackerService.startTrackPoll(message.clan_id, message.channel_id, sent.message_id);
   }
 }
