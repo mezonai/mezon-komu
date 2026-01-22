272dc53fcda139220935772b3e14a51a1b24e010
michael.mai <michael.mai.thatera@gmail.com>
2026-01-22 16:56:22 +0700

    add command list event

--- src/bot/asterisk-commands/commands/opentalk/opentalk.command.ts
+++ src/bot/asterisk-commands/commands/opentalk/opentalk.command.ts
@@ -2,7 +2,7 @@ import { ChannelMessage } from 'mezon-sdk';
 import { Command } from 'src/bot/base/commandRegister.decorator';
 import { CommandMessage } from '../../abstracts/command.abstract';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/bot/models';
+import { EventMezon, User } from 'src/bot/models';
 import { Repository } from 'typeorm';
 import {
   OpentalkService,
@@ -16,6 +16,8 @@ export class OpentalkCommand extends CommandMessage {
     private voiceSessionTrackingService: VoiceSessionTrackingService,
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    @InjectRepository(EventMezon)
+    private readonly eventRepo: Repository<EventMezon>,
   ) {
     super();
   }
@@ -34,11 +36,51 @@ export class OpentalkCommand extends CommandMessage {
   }
 
   async execute(args: string[], message: ChannelMessage) {
+    if (message.sender_id !== '1827994776956309504') return;
+    if (args[0] === 'list') {
+      const events = await this.eventRepo
+        .createQueryBuilder('e')
+        .orderBy('e.id', 'DESC')
+        .limit(15)
+        .getMany();
+
+      if (!events.length) {
+        const text = 'Không có events!';
+        return this.replyMessageGenerate(
+          {
+            messageContent: text,
+            mk: [{ type: 'pre', s: 0, e: text.length }],
+          },
+          message,
+        );
+      }
+
+      const result = events
+        .map((e, i) => {
+          const raw = e.timeStart;
+          const time = Number(raw);
+          const date = new Date(time);
+          return `${i + 1}. ${e.id} | ${e.title} | ${new Date(date).toLocaleString()} | ${e.channelVoiceId ?? 'không có'} `;
+        })
+        .join('\n');
+
+      return this.replyMessageGenerate(
+        {
+          messageContent: result,
+          mk: [{ type: 'pre', s: 0, e: result.length }],
+        },
+        message,
+      );
+    }
+
     if (args[0] === 'up') {
-      if (message.sender_id !== '1827994776956309504') return;
       const userId = args[1];
       const min = +args[2];
-      await this.voiceSessionTrackingService.adjustUserVoiceSessionTime(userId, min, UpdateTimeType.UP);
+      await this.voiceSessionTrackingService.adjustUserVoiceSessionTime(
+        userId,
+        min,
+        UpdateTimeType.UP,
+      );
       const messageContent = `Tăng ${min} success! ${userId}`;
       return this.replyMessageGenerate(
         {
@@ -50,10 +92,13 @@ export class OpentalkCommand extends CommandMessage {
     }
 
     if (args[0] === 'down') {
-      if (message.sender_id !== '1827994776956309504') return;
       const userId = args[1];
       const min = +args[2];
-      await this.voiceSessionTrackingService.adjustUserVoiceSessionTime(userId, min, UpdateTimeType.DOWN);
+      await this.voiceSessionTrackingService.adjustUserVoiceSessionTime(
+        userId,
+        min,
+        UpdateTimeType.DOWN,
+      );
       const messageContent = `Giảm ${min} success! ${userId}`;
       return this.replyMessageGenerate(
         {
@@ -64,6 +109,6 @@ export class OpentalkCommand extends CommandMessage {
       );
     }
 
-    return
+    return;
   }
 }
